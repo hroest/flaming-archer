hroest/flaming-archer

commit 72307f07acd2e09ccffcd81deff1e9fbf6d8b3a0

[FEATURE] added advanced option to extract rank ranges from idXML in IDFilter
git-svn-id: https://open-ms.svn.sourceforge.net/svnroot/open-ms/OpenMS@9946 6adb6e08-d915-0410-941f-83917bcadc18

include/OpenMS/FILTERING/ID/IDFilter.h
@@ -168,6 +168,47 @@ namespace OpenMS
 				}
 			}
 
+      /**
+        @brief filters a ProteinIdentification or PeptideIdentification corresponding to the score.
+
+        If the method higherScoreBetter() returns true for the IdentificationType the
+        n to m highestscoring hits are kept. Otherwise the n to m lowest scoring hits are kept.
+        This method is useful if a range of higher hits are used for decoy fairness analysis.
+      */
+      template <class IdentificationType>
+      void filterIdentificationsByBestNToMHits(const IdentificationType& identification, Size n, Size m, IdentificationType& filtered_identification)
+      {
+        if (n > m)
+        {
+          std::swap<Size>(n,m);
+        }
+
+        typedef typename IdentificationType::HitType HitType;
+        std::vector<HitType> filtered_hits;
+
+        IdentificationType temp_identification = identification;
+        temp_identification.sort(); // .. by score
+
+        filtered_identification = identification;
+        filtered_identification.setHits(std::vector<HitType>());
+
+        const std::vector<HitType>& hits = temp_identification.getHits();
+        for(Size i = n - 1; n <= m - 1; ++i)
+        {
+          if (i >= hits.size() || i < 0)
+          {
+            break;
+          }
+          filtered_hits.push_back(hits[i]);
+        }
+
+        if ( !filtered_hits.empty() )
+        {
+          filtered_identification.setHits(filtered_hits);
+          filtered_identification.assignRanks();
+        }
+      }
+
       /// filters a PeptideIdentification keeping only the best scoring hits (if strict is set, keeping only the best hit only if it is the only hit with that score)
 			void filterIdentificationsByBestHits(const PeptideIdentification& identification, PeptideIdentification& filtered_identification, bool strict = false);
 

include/OpenMS/APPLICATIONS/TOPPBase.h
@@ -876,6 +876,9 @@ namespace OpenMS
       /// Helper function that parses a range string ([a]:[b]) into two variables
       void parseRange_( const String& text, double& low, double& high ) const;
 
+      /// Helper function that parses a range string ([a]:[b]) into two variables
+      void parseRange_(const String& text, Int& low, Int& high) const;
+
       ///Type of progress logging
       ProgressLogger::LogType log_type_;
 

source/APPLICATIONS/TOPP/IDFilter.C
@@ -201,6 +201,7 @@ protected:
     setMinInt_("best:n_protein_hits", 0);
     registerFlag_("best:strict","Keep only the highest scoring peptide hit.\n"
                   "Similar to n_peptide_hits=1, but if there are two or more highest scoring hits, none are kept.");
+    registerStringOption_("best:n_to_m_peptide_hits","[min]:[max]",":","peptide hit rank range to extracts", false, true);
 		registerIntOption_("min_length","<integer>", 0, "Keep only peptide hits with a length greater or equal this value.", false);
 		setMinInt_("min_length", 0);
 		registerIntOption_("min_charge","<integer>", 1, "Keep only peptide hits for tandem spectra with charge greater or equal this value.", false);
@@ -251,6 +252,20 @@ protected:
 
 		Int best_n_peptide_hits = getIntOption_("best:n_peptide_hits");
 		Int best_n_protein_hits = getIntOption_("best:n_protein_hits");
+
+    Int best_n_to_m_peptide_hits_n = 0;
+    Int best_n_to_m_peptide_hits_m = numeric_limits<Size>::max();
+
+    //convert bounds to numbers
+    try
+    {
+      parseRange_(getStringOption_("best:n_to_m_peptide_hits"), best_n_to_m_peptide_hits_n, best_n_to_m_peptide_hits_m);		}
+    catch(Exception::ConversionError&)
+    {
+      writeLog_("Invalid boundary '" + getStringOption_("best:n_to_m_peptide_hits") + "' given. Aborting!");
+      printUsage_();
+      return ILLEGAL_PARAMETERS;
+    }
 
 		bool best_strict = getFlag_("best:strict");
 		UInt min_length = getIntOption_("min_length");
@@ -435,6 +450,13 @@ protected:
 				PeptideIdentification temp_identification = filtered_identification;
 				filter.filterIdentificationsByBestNHits(temp_identification, best_n_peptide_hits, filtered_identification);
 			}
+
+      if (best_n_to_m_peptide_hits_m != numeric_limits<Size>::max() || best_n_to_m_peptide_hits_n != 0)
+      {
+        applied_filters.insert("Filtering by best n to m peptide hits ...\n");
+        PeptideIdentification temp_identification = filtered_identification;
+        filter.filterIdentificationsByBestNToMHits(temp_identification, best_n_to_m_peptide_hits_n, best_n_to_m_peptide_hits_m, filtered_identification);
+      }
 
       if (mz_error_filtering)
       {

source/APPLICATIONS/TOPPBase.C
@@ -1874,6 +1874,21 @@ namespace OpenMS
 		}
 	}
 
+  void TOPPBase::parseRange_(const String& text, Int& low, Int& high) const
+  {
+    String tmp = text.prefix(':');
+    if (tmp!="")
+    {
+      low = tmp.toInt();
+    }
+    tmp = "";
+    tmp = text.suffix(':');
+    if (tmp!="")
+    {
+      high = tmp.toInt();
+    }
+  }
+
 	Param TOPPBase::getSubsectionDefaults_(const String& /*section*/) const
 	{
 		throw NotImplemented(__FILE__,__LINE__,__PRETTY_FUNCTION__);

commit 3625b0d73ebf6a4d08406d87a650631a1c29ccae

[FIX, INTERNAL] * compile error for VS2005 in IDFilter.h fixed introduced in r9946
git-svn-id: https://open-ms.svn.sourceforge.net/svnroot/open-ms/OpenMS@10004 6adb6e08-d915-0410-941f-83917bcadc18

include/OpenMS/FILTERING/ID/IDFilter.h
@@ -35,6 +35,7 @@
 #ifndef OPENMS_FILTERING_ID_IDFILTER_H
 #define OPENMS_FILTERING_ID_IDFILTER_H
 
+#include <OpenMS/config.h>
 #include <OpenMS/DATASTRUCTURES/String.h>
 #include <OpenMS/METADATA/ProteinIdentification.h>
 #include <OpenMS/KERNEL/MSExperiment.h>
@@ -187,7 +188,7 @@ namespace OpenMS
       {
         if (n > m)
         {
-          std::swap<Size>(n,m);
+          std::swap(n,m);
         }
 
         typedef typename IdentificationType::HitType HitType;